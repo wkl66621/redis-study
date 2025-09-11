1abcf9eedd48936a40b6dc44a125f764ce6552d2
wkl66621 <2546160781@qq.com>
2025-09-11 13:45:47 +0800

    新建项目2

--- service/src/main/java/com/wkl/study/redis/redisstudy/service/mysqlDemoService.java
+++ service/src/main/java/com/wkl/study/redis/redisstudy/service/mysqlDemoService.java
@@ -1,11 +1,12 @@
 package com.wkl.study.redis.redisstudy.service;
 
 import com.wkl.study.redis.redisstudy.dataBase.mapper.InfoToHtmlMapper;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class mysqlDemoService {
-    @Autowired
+    @Resource
     private InfoToHtmlMapper infoToHtmlMapper;
 }
