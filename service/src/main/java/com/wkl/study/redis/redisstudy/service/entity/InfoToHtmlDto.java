c1d41a11cabb958702ffcfee9bd7c91aba796bdb
wkl66621 <2546160781@qq.com>
2025-09-14 15:39:19 +0800

    新建项目3

--- service/src/main/java/com/wkl/study/redis/redisstudy/service/entity/InfoToHtmlDto.java
+++ service/src/main/java/com/wkl/study/redis/redisstudy/service/entity/InfoToHtmlDto.java
@@ -3,12 +3,12 @@ package com.wkl.study.redis.redisstudy.service.entity;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.Serializable;
 
-
-@Getter
-@Setter
+@Data
+@Slf4j
 public class InfoToHtmlDto implements Serializable {
     private Integer id;
 
