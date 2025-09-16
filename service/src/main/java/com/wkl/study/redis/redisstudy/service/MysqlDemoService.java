3186561156d126b770217dee3c5c8ac970bfd625
wkl66621 <2546160781@qq.com>
2025-09-16 16:54:50 +0800

    若redis中存在key则不写入

--- service/src/main/java/com/wkl/study/redis/redisstudy/service/MysqlDemoService.java
+++ service/src/main/java/com/wkl/study/redis/redisstudy/service/MysqlDemoService.java
@@ -5,12 +5,15 @@ import com.wkl.study.redis.redisstudy.dataBase.po.InfoToHtmlPo;
 import com.wkl.study.redis.redisstudy.dataBase.vo.InfoToHtmlVo;
 import com.wkl.study.redis.redisstudy.service.entity.InfoToHtmlDto;
 import com.wkl.study.redis.redisstudy.service.repositoryImpl.InfoToHtmlRepositoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MysqlDemoService {
+    private static final Logger log = LoggerFactory.getLogger(MysqlDemoService.class);
     @Autowired
     InfoToHtmlRepositoryImpl infoToHtmlRepositoryImpl;
 
@@ -25,8 +28,12 @@ public class MysqlDemoService {
         //从数据库中去取出数据
         infoToHtmlPo = infoToHtmlRepositoryImpl.selectById(infoToHtmlDto);
         InfoToHtmlVo infoToHtmlVo = InfoToHtmlPoToVoAssemble.getInstance().convert(infoToHtmlPo);
-        //将数据存入redis中
-        redisTemplate.opsForValue().set("infoToHtmlVo",infoToHtmlVo);
-        System.out.println(infoToHtmlPo);
+        if(redisTemplate.opsForValue().getOperations().hasKey("infoToHtmlVo")){
+            log.info("redis中已存在key:{}","infoToHtmlVo");
+        }else{
+            //将数据存入redis中
+            redisTemplate.opsForValue().set("infoToHtmlVo",infoToHtmlVo);
+        }
+        log.info("{}",infoToHtmlPo);
     }
 }
