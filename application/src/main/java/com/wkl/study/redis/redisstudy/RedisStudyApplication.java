dafb5ebd443a1b4a8df01af001c1b56df039d361
wkl66621 <2546160781@qq.com>
2025-09-16 11:59:29 +0800

    Application完善，增加.run方法阻塞线程

--- application/src/main/java/com/wkl/study/redis/redisstudy/RedisStudyApplication.java
+++ application/src/main/java/com/wkl/study/redis/redisstudy/RedisStudyApplication.java
@@ -1,13 +1,18 @@
 package com.wkl.study.redis.redisstudy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.wkl.study.redis.redisstudy.dataBase.mapper")
 @SpringBootApplication
+@Slf4j
 public class RedisStudyApplication {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        SpringApplication.run(RedisStudyApplication.class, args);
+        log.info("RedisStudyApplication服务启动成功----------------->耗时：{}", System.currentTimeMillis() - start);
     }
 
 }
